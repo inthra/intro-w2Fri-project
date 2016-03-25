a13568636a9bb81454645a6d9477b4ac164e3b50
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-03-25 14:11:04 -0700

    add Question 5 and Scripts

--- js/scripts.js
+++ js/scripts.js
@@ -5,45 +5,46 @@ $(document).ready(function(){
     var q2 = $("input:radio[name=question2]:checked").val();
     var q3 = $("input:radio[name=question3]:checked").val();
     var q4 = $("input:radio[name=question4]:checked").val();
+    var q5 = $("input:radio[name=question5]:checked").val();
 
 //Branching combination of Question 1,2, and 3 ------------------------
-    if (q1 && q2 && q3 && q4) {
+    if (q1 && q2 && q3 && q4 && q5) {
       if (q1 === "city" && q2 === "high" && q3 === "tourist") {
         $("#destination").empty().append("Las Vegas");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "beach" && q2 === "high" && q3 === "tourist") {
         $("#destination").empty().append("Hawaii");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "nature" && q2 === "high" && q3 === "tourist") {
         $("#destination").empty().append("Iceland");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "city" && q2 === "low" && q3 === "tourist") {
         $("#destination").empty().append("New Orleans");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "beach" && q2 === "low" && q3 === "tourist") {
         $("#destination").empty().append("Bali");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "nature" && q2 === "low" && q3 === "tourist") {
         $("#destination").empty().append("Yosemite National Park");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "city" && q2 === "high" && q3 === "traveler") {
         $("#destination").empty().append("London");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "beach" && q2 === "high" && q3 === "traveler") {
         $("#destination").empty().append("Cancun, Mexico");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "nature" && q2 === "high" && q3 === "traveler") {
         $("#destination").empty().append("Iceland and Greenland");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "city" && q2 === "low" && q3 === "traveler") {
         $("#destination").empty().append("Brisbane, Australia");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "beach" && q2 === "low" && q3 === "traveler") {
         $("#destination").empty().append("Costa Rica");
-        $("#result").show();
+        $("#result").fadeIn().show();
       } else if (q1 === "nature" && q2 === "low" && q3 === "traveler") {
         $("#destination").empty().append("Yellowstone National Park");
-        $("#result").show();
+        $("#result").fadeIn().show();
       }
 
     } else {
@@ -51,25 +52,45 @@ $(document).ready(function(){
     }
 
 //Branching combination of Question 2 and 4 ------------------------
-    if (q1 && q2 && q3 && q4) {
+    if (q1 && q2 && q3 && q4 && q5) {
       if (q2 === "high" && q4 === "high") {
-        $("#accommodation").empty().append("Hotel1");
-        $("#result").show();
+        $("#accommodation1").show();
       } else if (q2 === "high" && q4 === "medium") {
-        $("#accommodation").empty().append("Hotel2");
-        $("#result").show();
+        $("#accommodation1").show();
       } else if (q2 === "high" && q4 === "low") {
-        $("#accommodation").empty().append("Hotel3");
-        $("#result").show();
+        $("#accommodation2").show();
       } else if (q2 === "low" && q4 === "high") {
-        $("#accommodation").empty().append("Hotel4");
-        $("#result").show();
+        $("#accommodation1").show();
       } else if (q2 === "low" && q4 === "medium") {
-        $("#accommodation").empty().append("Hotel5");
-        $("#result").show();
+        $("#accommodation1").show();
       } else if (q2 === "low" && q4 === "low") {
-        $("#accommodation").empty().append("Hotel6");
-        $("#result").show();
+        $("#accommodation2").show();
+      }
+
+    } else {
+      $("#warning").empty().append("Please Answer All Questions");
+    }
+
+//Branching combination of Question 3 and 5 ------------------------
+    if (q1 && q2 && q3 && q4 && q5) {
+      if (q3 === "tourist" && q5 === "one") {
+        $("#destination2").empty().append("2nd Option - Istanbul, Turkey");
+        $("#result").fadeIn().show();
+      } else if (q3 === "tourist" && q5 === "few") {
+        $("#destination2").empty().append("2nd Option - Morocco");
+        $("#result").fadeIn().show();
+      } else if (q3 === "tourist" && q5 === "group") {
+        $("#destination2").empty().append("2nd Option - Paris, France");
+        $("#result").fadeIn().show();
+      } else if (q3 === "traveler" && q5 === "one") {
+        $("#destination2").empty().append("2nd Option - Amsterdam");
+        $("#result").fadeIn().show();
+      } else if (q3 === "traveler" && q5 === "few") {
+        $("#destination2").empty().append("2nd Option - Cambodia");
+        $("#result").fadeIn().show();
+      } else if (q3 === "traveler" && q5 === "group") {
+        $("#destination2").empty().append("2nd Option - Italy");
+        $("#result").fadeIn().show();
       }
 
     } else {
