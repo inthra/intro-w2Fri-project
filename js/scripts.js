c056fecda9c2e99a24389d5d3f02d4d90aba8138
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-03-25 10:05:07 -0700

    edited Question 2 and added Scripts for Question 2

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,29 @@ $(document).ready(function(){
     var q1 = $("input:radio[name=question1]:checked").val();
     var q2 = $("input:radio[name=question2]:checked").val();
 
-    if (q1 === "city") {
-      $("#destination").empty().append("New York City");
-      $("#result").show();
-    } else if (q1 === "beach") {
-      $("#destination").empty().append("Bahamas");
-      $("#result").show();
-    } else  {
-      $("#destination").empty().append("Iceland");
+    if (q1 && q2) {
+      if (q1 === "city" && q2 === "yes") {
+        $("#destination").empty().append("Las Vegas");
+        $("#result").show();
+      } else if (q1 === "beach" && q2 === "yes") {
+        $("#destination").empty().append("Hawaii");
+        $("#result").show();
+      } else if (q1 === "nature" && q2 === "yes") {
+        $("#destination").empty().append("Iceland");
+        $("#result").show();
+      } else if (q1 === "city" && q2 === "no") {
+        $("#destination").empty().append("New Orleans");
+        $("#result").show();
+      } else if (q1 === "beach" && q2 === "no") {
+        $("#destination").empty().append("Bali");
+        $("#result").show();
+      } else if (q1 === "nature" && q2 === "no") {
+        $("#destination").empty().append("Yosemite National Park");
+        $("#result").show();
+      }
+
+    } else {
+      $("#destination").empty().append("Please Answer All Questions");
       $("#result").show();
     }
 
