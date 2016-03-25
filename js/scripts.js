1b75a0739f04c7ea527ae6c5b62e80f3e0a6a06a
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-03-25 15:20:35 -0700

    add FadeOut scripts to show only the destination

--- js/scripts.js
+++ js/scripts.js
@@ -12,39 +12,51 @@ $(document).ready(function(){
       if (q1 === "city" && q2 === "high" && q3 === "tourist") {
         $("#destination").empty().append("Las Vegas");
         $("#result").fadeIn().show();
+        $("#spot2, #spot3, #spot4, #spot5, #spot6, #spot7, #spot8, #spot9, #spot10, #spot11, #spot12").fadeOut();
       } else if (q1 === "beach" && q2 === "high" && q3 === "tourist") {
         $("#destination").empty().append("Hawaii");
         $("#result").fadeIn().show();
+        $("#spot1, #spot3, #spot4, #spot5, #spot6, #spot7, #spot8, #spot9, #spot10, #spot11, #spot12").fadeOut();
       } else if (q1 === "nature" && q2 === "high" && q3 === "tourist") {
         $("#destination").empty().append("Iceland");
         $("#result").fadeIn().show();
+        $("#spot1, #spot2, #spot4, #spot5, #spot6, #spot7, #spot8, #spot9, #spot10, #spot11, #spot12").fadeOut();
       } else if (q1 === "city" && q2 === "low" && q3 === "tourist") {
         $("#destination").empty().append("New Orleans");
         $("#result").fadeIn().show();
+        $("#spot1, #spot2, #spot3, #spot5, #spot6, #spot7, #spot8, #spot9, #spot10, #spot11, #spot12").fadeOut();
       } else if (q1 === "beach" && q2 === "low" && q3 === "tourist") {
         $("#destination").empty().append("Bali");
         $("#result").fadeIn().show();
+        $("#spot1, #spot2, #spot3, #spot4, #spot6, #spot7, #spot8, #spot9, #spot10, #spot11, #spot12").fadeOut();
       } else if (q1 === "nature" && q2 === "low" && q3 === "tourist") {
         $("#destination").empty().append("Yosemite National Park");
         $("#result").fadeIn().show();
+        $("#spot1, #spot2, #spot3, #spot4, #spot5, #spot7, #spot8, #spot9, #spot10, #spot11, #spot12").fadeOut();
       } else if (q1 === "city" && q2 === "high" && q3 === "traveler") {
         $("#destination").empty().append("London");
         $("#result").fadeIn().show();
+        $("#spot1, #spot2, #spot3, #spot4, #spot5, #spot6, #spot8, #spot9, #spot10, #spot11, #spot12").fadeOut();
       } else if (q1 === "beach" && q2 === "high" && q3 === "traveler") {
         $("#destination").empty().append("Cancun, Mexico");
         $("#result").fadeIn().show();
+        $("#spot1, #spot2, #spot3, #spot4, #spot5, #spot6, #spot7, #spot9, #spot10, #spot11, #spot12").fadeOut();
       } else if (q1 === "nature" && q2 === "high" && q3 === "traveler") {
         $("#destination").empty().append("Iceland and Greenland");
         $("#result").fadeIn().show();
+        $("#spot1, #spot2, #spot3, #spot4, #spot5, #spot6, #spot7, #spot8, #spot10, #spot11, #spot12").fadeOut();
       } else if (q1 === "city" && q2 === "low" && q3 === "traveler") {
         $("#destination").empty().append("Brisbane, Australia");
         $("#result").fadeIn().show();
+        $("#spot1, #spot2, #spot3, #spot4, #spot5, #spot6, #spot7, #spot8, #spot9, #spot11, #spot12").fadeOut();
       } else if (q1 === "beach" && q2 === "low" && q3 === "traveler") {
         $("#destination").empty().append("Costa Rica");
         $("#result").fadeIn().show();
+        $("#spot1, #spot2, #spot3, #spot4, #spot5, #spot6, #spot7, #spot8, #spot9, #spot10, #spot12").fadeOut();
       } else if (q1 === "nature" && q2 === "low" && q3 === "traveler") {
         $("#destination").empty().append("Yellowstone National Park");
         $("#result").fadeIn().show();
+        $("#spot1, #spot2, #spot3, #spot4, #spot5, #spot6, #spot7, #spot8, #spot9, #spot10, #spot11").fadeOut();
       }
 
     } else {
@@ -97,9 +109,7 @@ $(document).ready(function(){
       $("#warning").empty().append("Please Answer All Questions");
     }
 
+
     event.preventDefault();
   });
 });
-
-//$("#celebrity").empty().append(name);
-//$("#your-date").show();
